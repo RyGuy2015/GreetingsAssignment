b8141793b9a094fad8ab00c27cce288feb749edf
Ryan Ingram <ry_ingram@hotmail.com>
2016-04-21 22:15:04 -0400

    This is the initial commit of the project

---
+++ GreetingsPage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Greetings
+{
+    class GreetingsPage : ContentPage
+    {
+        public GreetingsPage()
+        {
+            Content = new Label
+            {
+                Text = "Greetings, Xamarin.Forms!",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                XAlign = TextAlignment.Center,
+                YAlign = TextAlignment.Center
+            };
+            // Padding = new Thickness(0, 20, 0, 0);
+            Padding = Device.OnPlatform(new Thickness(0, 20, 0, 0), new Thickness(0), new Thickness(0));
+            
+        }
+       
+    }
+}
